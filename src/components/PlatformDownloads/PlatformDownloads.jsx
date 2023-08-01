53204693b388eaf008e66438c5d3b41a743534ad
Artem Vasylenko <irbiskronos@gmail.com>
2023-08-01 12:35:10 +0300

    downloads platform in progress

--- src/components/PlatformDownloads/PlatformDownloads.jsx
+++ src/components/PlatformDownloads/PlatformDownloads.jsx
@@ -1,21 +1,122 @@
 import React from 'react'
+import clsx from 'clsx';
 import styles from './PlatformDownloads.module.scss';
+import SvgWindowsIcon from '@site/static/img/icons/windows_icon.svg';
+import SvgMacIcon from '@site/static/img/icons/mac_icon.svg';
+import SvgDebIcon from '@site/static/img/icons/deb_icon.svg';
+import SvgRpmIcon from '@site/static/img/icons/rpm_icon.svg';
+import Link from "@docusaurus/Link";
+import Translate from "@docusaurus/Translate";
+
 
 export default function PlatformDownloads({ featured }) {
-    const handleDownload = (link) => {
+
+    /* const handleDownload = (link) => {
         window.open(link);
+    } 
+    onClick={() => handleDownload(link)}
+    */
+
+    function DownloadWindows({ url, name }) {
+        return (
+            <div className={clsx('icon', styles.icon_windows)}>
+                <Link to={url}>
+                    <Translate
+                        id="com.platform.windows"
+                        description="Windows Platform">
+                        Windows
+                    </Translate>
+                </Link>
+                <div className={styles.link}>{name}</div>
+            </div>
+        )
+    }
+
+    function DownloadMac({ url, name }) {
+        return (
+            <div className={clsx('icon', styles.icon_mac)}>
+                <Link to={url}>
+                    <Translate
+                        id="com.platform.mac"
+                        description="Mac Platform">
+                        MacOS
+                    </Translate>
+                </Link>
+                <div className={styles.link}>{name}</div>
+            </div>
+        )
+    }
+
+    function DownloadLinuxDeb({ url, name }) {
+        return (
+            <div className={clsx('icon', styles.icon_debian)}>
+                <Link to={url}>
+                    <Translate
+                        id="com.platform.deb"
+                        description="Debian Platform">
+                        Debian
+                    </Translate>
+                </Link>
+                <div className={styles.link}>{name}</div>
+            </div>
+        )
+    }
+
+    function DownloadLinuxRpm({ url, name }) {
+        return (
+            <div className={clsx('icon', styles.icon_fedora)}>
+                <Link to={url}>
+                    <Translate
+                        id="com.platform.fedora"
+                        description="Fedora Platform">
+                        Fedora
+                    </Translate>
+                </Link>
+                <div className={styles.link}>{name}</div>
+            </div>
+        )
+    }
+
+    const components = {
+        windows: DownloadWindows,
+        mac: DownloadMac,
+        deb: DownloadLinuxDeb,
+        rpm: DownloadLinuxRpm,
+    }
+
+    const icons = {
+        windows: () => (<SvgWindowsIcon />),
+        mac: () => (<SvgMacIcon />),
+        deb: () => (<SvgDebIcon />),
+        rpm: () => (<SvgRpmIcon />),
+    };
+
+    const fullNames = {
+        win: 'Windows',
+        mac: 'MacOS',
+        deb: 'Debian',
+        rpm: 'Fedora',
     }
 
     return (
         <>
-
             <div className={styles.wrapper}>
                 <div className={styles.container}>
-                    {featured.map((file, index) => (
-                        <div key={file.type} className={styles.flexItem} onClick={() => handleDownload(file.link)}>
-                            <div className={styles.title}>{file.type}</div>
-                        </div>
-                    ))}
+                    {featured.map((value) => {
+                        const { type, link, fileName } = value;
+                        const IconComponent = icons[type];
+                        const DownloadComponent = components[type];
+                        return (
+                            <div
+                                key={type}
+                                className={styles.flexItem}
+                            >
+                                <IconComponent />
+                                <DownloadComponent url={link} name={fileName} />
+                            </div>
+                        )
+                    })}
+
                 </div>
             </div>
         </>
