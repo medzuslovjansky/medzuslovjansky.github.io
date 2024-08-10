cfb9d09639104b90e8eac8ac5297cb7bda2cd9de
Yaroslav Serhieiev <noomorph@gmail.com>
2024-08-10 16:37:27 +0300

    fix: issue with li

--- src/utils/extractListItems.js
+++ src/utils/extractListItems.js
@@ -5,7 +5,10 @@ export function extractListItems(children) {
 
   const onlyChild = React.Children.only(children);
   const allRows = React.Children.toArray(onlyChild.props.children);
-  const liRows = allRows.filter(item => item.type?.name === 'MDXLi');
+  const liRows = allRows.filter(item => {
+    const name = item.type?.name;
+    return name === 'MDXLi' || name === 'li';
+  });
 
   return liRows;
 }
